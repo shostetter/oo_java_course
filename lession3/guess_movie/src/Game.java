67463b3c8aca03e31895a2cf7aefc1baea49eecd
Seth Hostetter <51931648+shostetter@users.noreply.github.com>
2021-09-08 17:00:53 -0400

    left off
    needs to be rethought i dont think i need the array

--- lession3/guess_movie/src/Game.java
+++ lession3/guess_movie/src/Game.java
@@ -9,37 +9,41 @@ public class Game {
         File file = new File("movies.txt");
 
         MovieList movieList = new MovieList();
-
-
-
+        // read input file
+        // Todo: move this to seperate class
         if (file.exists()) {
-            Scanner scanner  = new Scanner(file);
+            Scanner scanner = new Scanner(file);
             int movies = 0;
             while (scanner.hasNextLine()){
                 String line = scanner.nextLine(); // each line in input file
                 Movie movie = new Movie();
                 movieList.addMovie(movie);
 
-                // Set the fields
+                // Set the title and array of chars
                 movie.name = line;
-//                movie.letters = {"t","e","s", "t"}
+                movie.getLetters();
                 movies += 1;
 
             }
-
             int randomNumber = (int) (Math.random() * movies) + 1;
-            System.out.println("The input file has " + movies + " movies");
-            System.out.println(randomNumber);
+//            System.out.println("The input file has " + movies + " movies");
 
             Movie movieToGuess = movieList.searchMovieByIndex(randomNumber);
 
             System.out.println(movieToGuess.name);
             System.out.println(movieToGuess.hideLetters());
-            movieToGuess.getLetters();
             System.out.println(movieToGuess.letters);
-//            System.out.println(movieToGuess.letters);
-//            Arrays.toString(movieToGuess.letters);
 
+            Scanner inScanner = new Scanner(System.in); // new scanner inst
+            for (int i = 10; i > 0; i--) {
+                System.out.println("You have "+i+" guesses remaining...");
+                String guess = inScanner.nextLine();
+                System.out.println(guess+"...");
+                boolean found = Arrays.asList(movieToGuess.letters).contains(guess.charAt(0));
+                System.out.println(found);
+                System.out.println(movieToGuess.letters);
+
+            }
 
         }
     }
--- lession3/guess_movie/src/Movie.java
+++ lession3/guess_movie/src/Movie.java
@@ -17,8 +17,8 @@ public class Movie {
         this.letters = new Character[this.name.length()];
         System.out.println(this.name.length());
         for (int i=0; i< this.name.length(); i++){
-//            System.out.println(this.name.charAt(i));
-//            System.out.println(i);
+            System.out.println(this.name.charAt(i));
+            System.out.println(i);
             this.letters[i] =this.name.charAt(i);
         }
     }
