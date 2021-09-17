cfeff3fc0bbf0cb2ae62b6cedb160e35a1ae44d0
Seth Hostetter <51931648+shostetter@users.noreply.github.com>
2021-09-17 08:34:06 -0400

    minor flow enhancements

--- lesson4/chess/src/Queen.java
+++ lesson4/chess/src/Queen.java
@@ -2,14 +2,13 @@ public class Queen extends Piece{
     // override isvalid move for rook
     boolean isValidMove(Position newPosition){
         // call parent method
-        if(super.isValidMove(newPosition)) {
-            if(newPosition.column == this.position.column || newPosition.row == this.position.row){
-                return true;
-            } else if(Math.abs(newPosition.column - this.position.column) == Math.abs(newPosition.row - this.position.row)){
-                return true;
-            } else {
-                return false;
-            }
+        if(!super.isValidMove(newPosition)) {
+            return false;
+        }
+        if(newPosition.column == this.position.column || newPosition.row == this.position.row){
+            return true;
+        } else if(Math.abs(newPosition.column - this.position.column) == Math.abs(newPosition.row - this.position.row)){
+            return true;
         } else {
             return false;
         }
