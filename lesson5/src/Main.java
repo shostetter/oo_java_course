63dcc4e04bfcb4704f85a2c1128cf1b65db8df00
Seth Hostetter <51931648+shostetter@users.noreply.github.com>
2021-10-26 22:27:11 -0400

    adds readme

--- lesson5/src/Main.java
+++ lesson5/src/Main.java
@@ -3,6 +3,12 @@ import java.io.File;
 class Main {
 
     public static void main(String[] args) throws Exception {
+        //Create a Main class with the main method and start running the simulation:
+            //Create a Simulation object
+            //Load Items for Phase-1 and Phase-2
+            //Load a fleet of U1 rockets for Phase-1 and then for Phase-2
+            //Run the simulation using the fleet of U1 rockets and display the total budget required.
+            //        Repeat the same for U2 rockets and display the total budget for that.
 
         Simulation sim = new Simulation();
 
